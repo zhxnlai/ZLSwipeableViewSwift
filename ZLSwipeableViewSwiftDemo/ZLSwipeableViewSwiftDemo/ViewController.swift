8adb90b6e387401eef9337d26a187a233cb98d0f
Zhixuan Lai <zhxnlai@gmail.com>
2015-05-25 10:28:18 -0700

    update demo

--- ZLSwipeableViewSwiftDemo/ZLSwipeableViewSwiftDemo/ViewController.swift
+++ ZLSwipeableViewSwiftDemo/ZLSwipeableViewSwiftDemo/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import performSelector_swift
+import UIColor_FlatColors
+import Cartography
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var swipeableView: ZLSwipeableView!
 
     @IBAction func reload(sender: UIBarButtonItem) {
-        swipeableView.discardViews()
-        swipeableView.loadViews()
+        let alertController = UIAlertController(title: nil, message: "Load Cards:", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            // ...
+        }
+        alertController.addAction(cancelAction)
+        
+        let ProgrammaticallyAction = UIAlertAction(title: "Programmatically", style: .Default) { (action) in
+            self.loadCardsFromXib = false
+            self.colorIndex = 0
+            self.swipeableView.discardViews()
+            self.swipeableView.loadViews()
+        }
+        alertController.addAction(ProgrammaticallyAction)
+
+        let XibAction = UIAlertAction(title: "From Xib", style: .Default) { (action) in
+            self.loadCardsFromXib = true
+            self.colorIndex = 0
+            self.swipeableView.discardViews()
+            self.swipeableView.loadViews()
+        }
+        alertController.addAction(XibAction)
+
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     @IBAction func pushLeft(sender: UIBarButtonItem) {
@@ -33,36 +58,81 @@ class ViewController: UIViewController {
         swipeableView.swipeTopView(inDirection: .Down)
     }
     
+    var colors = ["Turquoise", "Green Sea", "Emerald", "Nephritis", "Peter River", "Belize Hole", "Amethyst", "Wisteria", "Wet Asphalt", "Midnight Blue", "Sun Flower", "Orange", "Carrot", "Pumpkin", "Alizarin", "Pomegranate", "Clouds", "Silver", "Concrete", "Asbestos"]
+    var colorIndex = 0
+    var loadCardsFromXib = false
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        swipeableView.layoutIfNeeded()
         swipeableView.loadViews()
     }
+    
+    // TODO: customize animation, direction
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var count = 0
-        swipeableView.nextView = {
-            var cardView = UIView(frame: self.swipeableView.bounds)
-            
-            var label = UILabel(frame: cardView.frame)
-            label.text = "# \(count)"
-            label.textColor = UIColor.blackColor()
-            label.numberOfLines = 0
-            cardView.addSubview(label)
-            
-            cardView.backgroundColor = UIColor.lightGrayColor()
-            cardView.layer.masksToBounds = false
-            cardView.layer.cornerRadius = 8
-            cardView.layer.shadowOffset = CGSizeMake(-5, 5)
-            cardView.layer.shadowRadius = 5
-            cardView.layer.shadowOpacity = 0.5
-            cardView.layer.shouldRasterize = true
-            
-            count++
-            return cardView
+
+        swipeableView.didStart = {view, location in
+            println("Did start swiping view at location: \(location)")
         }
-        //        swipeableView.didStart
+        swipeableView.swiping = {view, location, translation in
+            println("Swiping at view location: \(location) translation: \(translation)")
+        }
+        swipeableView.didEnd = {view, location in
+            println("Did end swiping view at location: \(location)")
+        }
+        swipeableView.didSwipe = {view, direction in
+            println("Did swipe view in direction: \(direction)")
+        }
+        swipeableView.didCancel = {view in
+            println("Did cancel swiping view")
+        }
+
+        swipeableView.nextView = {
+            if self.colorIndex < self.colors.count {
+                var cardView = CardView(frame: self.swipeableView.bounds)
+                cardView.backgroundColor = self.colorForName(self.colors[self.colorIndex])
+                self.colorIndex++
+                
+                if self.loadCardsFromXib {
+                    var contentView = NSBundle.mainBundle().loadNibNamed("CardContentView", owner: self, options: nil).first! as! UIView
+                    contentView.setTranslatesAutoresizingMaskIntoConstraints(false)
+                    contentView.backgroundColor = cardView.backgroundColor
+                    cardView.addSubview(contentView)
+                    
+                    // This is important:
+                    // https://github.com/zhxnlai/ZLSwipeableView/issues/9
+                    /*// Alternative:
+                    let metrics = ["width":cardView.bounds.width, "height": cardView.bounds.height]
+                    let views = ["contentView": contentView, "cardView": cardView]
+                    cardView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[contentView(width)]", options: .AlignAllLeft, metrics: metrics, views: views))
+                    cardView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[contentView(height)]", options: .AlignAllLeft, metrics: metrics, views: views))
+                    */
+                    layout(contentView, cardView) { view1, view2 in
+                        view1.left == view2.left
+                        view1.top == view2.top
+                        view1.width == cardView.bounds.width
+                        view1.height == cardView.bounds.height
+                    }
+                } else {
+                    var textView = UITextView(frame: cardView.bounds)
+                    textView.text = "This UITextView was created programmatically."
+                    textView.backgroundColor = UIColor.clearColor()
+                    textView.font = UIFont.systemFontOfSize(24)
+                    textView.editable = false
+                    textView.selectable = false
+                    cardView.addSubview(textView)
+                }
+                return cardView
+            }
+            return nil
+        }
+    }
+    
+    // MARK: ()
+    func colorForName(name: String) -> UIColor {
+        let sanitizedName = name.stringByReplacingOccurrencesOfString(" ", withString: "")
+        let selector = "flat\(sanitizedName)Color"
+        return UIColor.swift_performSelector(Selector(selector), withObject: nil) as! UIColor
     }
 }
 
