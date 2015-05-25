3a3449f53d55fc815d2f4f7ff2390a3500f674e8
Zhixuan Lai <zhxnlai@gmail.com>
2015-05-24 22:42:40 -0700

    adding card view

---
+++ ZLSwipeableViewSwiftDemo/ZLSwipeableViewSwiftDemo/CardView.swift
@@ -0,0 +1,35 @@
+//
+//  CardView.swift
+//  ZLSwipeableViewSwiftDemo
+//
+//  Created by Zhixuan Lai on 5/24/15.
+//  Copyright (c) 2015 Zhixuan Lai. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        // Shadow
+        layer.shadowColor = UIColor.blackColor().CGColor
+        layer.shadowOpacity = 0.33
+        layer.shadowOffset = CGSizeMake(0, 1.5)
+        layer.shadowRadius = 4.0
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.mainScreen().scale
+        
+        // Corner Radius
+        layer.cornerRadius = 10.0;
+    }
+}
