8d5f8291ef1a63cfdaef5076466bec6150fab696
Tim Broder <timothy.broder@gmail.com>
2015-07-15 14:21:32 -0400

    initializer fix for Xcode 7b3
    A non-failable initializer cannot chain to failable initializer 'init(coder:)' written with 'init?'

--- ZLSwipeableViewSwift/ZLSwipeableView.swift
+++ ZLSwipeableViewSwift/ZLSwipeableView.swift
@@ -257,7 +257,7 @@ public class ZLSwipeableView: UIView {
         setup()
     }
     
-    required public init(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
